e1ee7c085973c3f81cf3861b95d8aa4b71f248fd
강경미 <carami@nate.com>
2017-11-13 23:05:08 +0900

    no message

--- src/main/java/sunnyvale/examples/controller/MainController.java
+++ src/main/java/sunnyvale/examples/controller/MainController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import sunnyvale.examples.dto.DbInfo;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
